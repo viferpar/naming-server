3497dba01c8604219dc7c7a62bdedc238700edab
Vicent Ferrandiz <viferpar@gmail.com>
2021-09-25 12:38:05 +0200

    Initial commit

---
+++ src/test/java/es/viferpar/microservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.viferpar.microservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
